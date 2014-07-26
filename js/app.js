b8e83109282e31779fedddd8dd9535df371b8e4b
Stephanie McNeely <stefmcneely74@yahoo.com>
2014-07-26 11:19:01 -0500

    welcome

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 var triviaApp = angular.module('triviaApp', [])
+triviaApp.controller('QuestionListCtrl', function($scope){
+    $scope.questions = [
+        {
+            id: 1,
+            question: "Why is AngularJS named AndgularJS?",
+            option1: "It sounds cool.",
+            option2: "It describes HTML brackets",
+            option3: "The author had a habit of holding his head at an angle while coding.",
+            answer: "2"
+        }
+]
+})
