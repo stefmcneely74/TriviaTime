d2f788fbddead59354e34391ff81a6deb3d77a98
Stephanie McNeely <stefmcneely74@yahoo.com>
2014-07-26 15:11:25 -0500

    routes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
-var triviaApp = angular.module('triviaApp', [])
-triviaApp.controller('QuestionListCtrl', ['$scope', '$http',function($scope, $http){
-    $http.get('data/questions.json').success(function (data) {
-        $scope.questions = data;
-    });
+var triviaApp = angular.module('triviaApp', ['firebase', 'ngRoute']);
+triviaApp.config(['$routeProvider',function($routeProvider){
+    $routeProvider.
+        when('/questions', {
+        templateUrl: 'partials/question-list.html',
+            controller: 'QuestionListCtrl'
+        }).
+        otherwise({
+            redirectTo: '/questions'
+
+        });
+}]);
+
+triviaApp.controller('QuestionListCtrl', ['$scope', '$firebase', function($scope, $firebase){
+    var firebaseUrl= "https://samtriviaapp.firebaseio.com/questions";
+    //$http.get('data/questions.json').success(function (data) {//
+        $scope.questions = $firebase(new Firebase(firebaseUrl));
 }]);
