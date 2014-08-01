ad78289bf71afce4e63b025a9f562a3ab67597d5
Stephanie McNeely <stefmcneely74@yahoo.com>
2014-07-31 20:04:19 -0500

    resolved

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var triviaApp = angular.module('triviaApp', ['firebase', 'ngRoute']);
-triviaApp.config(['$routeProvider',function($routeProvider){
+triviaApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/questions', {
         templateUrl: 'partials/question-list.html',
             controller: 'QuestionListCtrl'
         }).
-        when('/questions/new',{
+        when('/questions/new', {
             templateUrl: 'partials/question-form.html',
             controller: 'QuestionNewCtrl'
         }).
@@ -20,12 +20,12 @@ triviaApp.controller('QuestionListCtrl', ['$scope', '$firebase', function($scope
     //$http.get('data/questions.json').success(function (data) {//
         $scope.questions = $firebase(new Firebase(firebaseUrl));
 }]);
-triviaApp.controller('QuestionNewCtrl', ['$scope','$firebase', '$location', function ($scope, $firebase, $location){
+triviaApp.controller('QuestionNewCtrl', ['$scope','$firebase', '$location', function ($scope, $firebase, $location) {
     $scope.question = {};
     $scope.persistQuestion = function(question) {
         var firebaseUrl = "https://samtriviaapp.firebaseio.com/questions";
         $scope.questions = $firebase(new Firebase(firebaseUrl));
-        $scope.questions.$add(questions).then(function(ref){
+        $scope.questions.$add(question).then(function(ref) {
             $location.url('/questions');
         });
     };
